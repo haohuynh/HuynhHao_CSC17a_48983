5a8e57750e48cc0b055462bea9421f23b0a75991
Hao Huynh <hah.ptit@gmail.com>
2015-10-14 17:18:57 -0700

    PokerGame Documentation : Complete Instroduction and Code Sample

--- upload.bat
+++ upload.bat
@@ -1,5 +1,5 @@
 git add --all
 git config --global user.name "Hao Huynh"
 git config --global user.email hah.ptit@gmail.com
-git commit -m "PokerGame: Document in process"
+git commit -m "PokerGame Documentation : Complete Instroduction and Code Sample"
 git push origin master
