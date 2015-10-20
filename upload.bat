2b356cf5d8ac0fa47bd7c348baeb9ca45a90f482
Hao Huynh <hah.ptit@gmail.com>
2015-10-19 21:29:03 -0700

    PokerGame Documentation : Completed

--- upload.bat
+++ upload.bat
@@ -1,5 +1,5 @@
 git add --all
 git config --global user.name "Hao Huynh"
 git config --global user.email hah.ptit@gmail.com
-git commit -m "PokerGame Documentation : Pseudocode completed"
+git commit -m "PokerGame Documentation : Completed"
 git push origin master
