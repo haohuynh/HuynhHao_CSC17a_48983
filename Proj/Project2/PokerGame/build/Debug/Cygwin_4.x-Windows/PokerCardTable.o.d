571f97a74534b88ddd0b40c25c8ca4cc15459137
Hao Huynh <hah.ptit@gmail.com>
2015-11-20 08:09:23 -0800

    Project2: Branched from Project1

---
+++ Proj/Project2/PokerGame/build/Debug/Cygwin_4.x-Windows/PokerCardTable.o.d
@@ -0,0 +1,6 @@
+build/Debug/Cygwin_4.x-Windows/PokerCardTable.o: PokerCardTable.cpp \
+ PokerCardTable.h PokerHelper.h
+
+PokerCardTable.h:
+
+PokerHelper.h:
---
+++ Proj/Project2/PokerGame/build/Debug/Cygwin_4.x-Windows/PokerHelper.o.d
@@ -0,0 +1,6 @@
+build/Debug/Cygwin_4.x-Windows/PokerHelper.o: PokerHelper.cpp \
+ PokerHelper.h PokerCardTable.h
+
+PokerHelper.h:
+
+PokerCardTable.h:
